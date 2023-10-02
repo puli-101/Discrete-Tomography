2baf5cd37373009408747dd7cb420925ba4bfbae
David Pulido <dpulidoc99@gmail.com>
2023-10-02 13:50:59 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    pass
+
+git init
+git add main.py
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:puli-101/Discrete-Tomography.git
+git push -u origin main
