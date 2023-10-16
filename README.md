c936a292d10ff6e5374fe4bcc0e2f34940b00bb0
David Pulido <dpulidoc99@gmail.com>
2023-10-16 14:59:48 +0200

    Linea coloreable?

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ On montre comment d´eterminer si, pour une sequence donnee, il est possible de 
 On generalise au cas d’une ligne dont la couleur de certaines cases est imposee.
 ### 1.3 Propagation
 On montre comment tirer parti de cet algorithme afin d’identifier des cases necessairement blanches ou noires dans une ligne/colonne, et on procede par propagation pour colorier partiellement une grille.
+
+
+### Q1 Si on a calcule tous les T[j,l]...
+Alors en particulier on a calcule T(M-1,k) qui nous dit s'il est possible de colorier les M cases de la ligne L_i avec la sequence (s1...sk) qui revient a la sequence de tous les k blocs qui doivent etre places a la ligne L_i. Autrement dit, avec T(M-1,k) on peut determiner si l'on peut colorier la ligne L_i.
+
--- main.py
+++ main.py
@@ -1,7 +1,47 @@
 from grid import *
 
+def line_is_colorable(ligne, j, l, T, s):
+    """
+        On considere la i-eme ligne 
+        les j+1 premiers cases de la i-eme ligne
+        et les l premiers blocks de s
+        T[a,b] = line_is_colorable()
+        retour : Vrai si on peut colorier les j+1 premiers cases d'une certaine ligne
+            avec les l premiers blocks de s
+    """
+    v = True
+    if l == 0:
+        #we dont have to place any block -> OK
+        v = True 
+    elif j < s[l - 1] - 1: 
+        #we're trying to place a block bigger than the space we have available
+        v = False
+    elif j == s[l - 1] - 1:
+        #if the current available size (j+1) matches the size of the last block of the sequence
+        #then we can color the line only if that's the only block in the sequence (i.e. l == 0)
+        v = l == 0
+    elif j > s[l - 1] - 1:
+        #si la case est blanche...
+        if ligne[j] == 0:
+            #!!!!!!!!!!!!!!!!!1
+            V = line_is_colorable(ligne, j - s[l - 1], l - 1, T, s)
+        else:
+            #d'apres l'enonce si la case est noire alors
+            #le dernier block de longueur s[l] est a la fin de la partie de la ligne
+            #il se termine a la ligne T[i,j]
+            v = True
+    return v 
+
+
+
 if __name__ == "__main__":
     #grille dans l'enonce
     G = Grid(4,5,[[3],[],[1,1,1],[3]], [[1,1],[1],[1,2],[1],[2]])
     G.print_grid()
+    for i in range(0,G.n_lignes):
+        l = len(G.contrainte_l[i])
+        T = []
+        for j in range(G.m_colonnes + 1):
+            T.append([0] * l)
+        print(str(i)+"-eme ligne coloreable ?",line_is_colorable(G.grid[i],G.m_colonnes-1,l,T,G.contrainte_l[i]))
 
