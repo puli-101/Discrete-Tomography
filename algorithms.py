a4d50e596036f770b2762b077c78479d7a0fc511
David Pulido <dpulidoc99@gmail.com>
2023-10-27 18:29:42 +0200

    Algoritmo 1.1

--- algorithms.py
+++ algorithms.py
@@ -8,7 +8,7 @@ class Solver:
         self.n = grid.n_lignes #access plus directe
         self.m = grid.m_colonnes
 
-    def line_is_colorable(self,ligne, j, l, s):
+    def line_is_colorable(self,ligne, T, j, l, s):
         """
             On considere la i-eme ligne 
             les j+1 premiers cases de la i-eme ligne
@@ -17,29 +17,33 @@ class Solver:
             retour : Vrai si on peut colorier les j+1 premiers cases d'une certaine ligne
                 avec les l premiers blocks de s
         """
-        v = True
         if l == 0:
             #we dont have to place any block -> OK
-            v = True 
+            T[j,l] = True 
         elif j < s[l - 1] - 1: 
             #we're trying to place a block bigger than the space we have available
-            v = False
+            T[j,l] = False
         elif j == s[l - 1] - 1:
             #if the current available size (j+1) matches the size of the last block of the sequence
             #then we can color the line only if that's the only block in the sequence (i.e. l == 0)
             #and there isn't any other block already
-            v = (l == 0 )
-            for x in ligne[:j+1]:
-                v = v and (x == 0)
+            T[j,l] = (l == 1)
         elif j > s[l - 1] - 1:
-            #si la case est blanche...
-            if ligne[j] == 0:
-                #!!!!!!!!!!!!!!!!!!
-                v = self.line_is_colorable(ligne, j - s[l - 1], l - 1, s)
+            #IL FAUT TESTER J > 0
+
+            blanche, noire = False
+            #CAS 1 On considere la case comme etant blanche
+            if (j-1,l) in T.keys(): 
+                blanche = T[j-1,l]
+            else: 
+                T[j-1,l] = blanche = (j - 1 >= 0) and self.line_is_colorable(ligne,T,j-1,l,s)
+            #CAS 2 On considere la case comme etant noire
+            if (j - s[l-1] - 1, l-1) in T.keys():
+                noire = T[(j - s[l-1] - 1, l-1)]
             else:
-                if ligne[j]==2 :
-                    v=self.line_is_colorable(ligne,j-s[l-1]-1,l-1,s)
-                else:
-                    v=self.line_is_colorable(ligne,j-s[l-1]-2,l-1,s)
-        return v 
+                T[(j - s[l-1] - 1, l-1)] = (j - s[l-1] - 1 >= 0) and self.line_is_colorable(ligne,T,j - s[l-1] - 1,l-1,s)
+                blanche = T[(j - s[l-1] - 1, l-1)]
+            #...
+            T[j,l] = blanche or noire 
+        return T[j,l]
     
